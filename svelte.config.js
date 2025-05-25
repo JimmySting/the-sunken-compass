e3cb6654238fd5cc95bc83f91ba10bd2b466219b
James Fitzpatrick <james.fitz33@gmail.com>
2025-05-25 17:15:34 -0400

    Build directory

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/the-sunken-compass' : ''
-		},
-		prerender: {
-			default: true
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
