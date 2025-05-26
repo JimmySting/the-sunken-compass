2789dd14a6a9c82d298f4f1ae6954d2a07b21eea
James Fitzpatrick <james.fitz33@gmail.com>
2025-05-25 20:37:29 -0400

    Updates path again

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/the-sunken-compass' : ''
-		}
+		})
 	}
 };
 
